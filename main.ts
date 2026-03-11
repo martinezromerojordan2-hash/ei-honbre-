a139224fe4b6db422441eeb5d9aa1178b9a1c53d
martinezromerojordan2-hash <martinezromero.jordan2@cedlavictoria.edu.co>
2026-03-11 10:44:16 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,37 @@
+for (let index = 0; index < 4; index++) {
+    basic.showIcon(IconNames.StickFigure)
+}
+for (let index = 0; index < 4; index++) {
+    basic.showIcon(IconNames.Sad)
+}
+basic.pause(100)
+for (let index = 0; index < 4; index++) {
+    basic.showIcon(IconNames.House)
+}
+basic.showString("POR QUE ESTAS TRISTE")
+for (let index = 0; index < 4; index++) {
+    basic.showIcon(IconNames.StickFigure)
+}
+basic.showString("ES QUE SOI PODRE")
+for (let index = 0; index < 4; index++) {
+    basic.showIcon(IconNames.House)
+}
+basic.showString("TE CONSEDERE UN DESEO")
+for (let index = 0; index < 4; index++) {
+    basic.showIcon(IconNames.StickFigure)
+    basic.showString("NOTE CREO AVER DESEO QUE TODO LO QUE TOQUE SE CONVIERTA EN ORO")
+    basic.showIcon(IconNames.House)
+}
+for (let index = 0; index < 4; index++) {
+    basic.showIcon(IconNames.House)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+}
+basic.forever(function () {
+	
+})
